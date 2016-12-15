889bbf88712c8bf3b0a8f81ef4489026a3d1dd9b
Takayuki Hasegawa <takayuki.hasegawa0311@gmail.com>
2016-12-15 13:43:37 +0900

    add spec

--- spec/hair/api/client_spec.rb
+++ spec/hair/api/client_spec.rb
@@ -1,11 +1,103 @@
 require 'spec_helper'
 
 describe Hair::Api::Client do
+
+  token = 'your token'
+
+  before do
+    Hair::Api::Client.configure do |options|
+      options[:token] = token
+    end
+    WebMock.enable!
+  end
+
   it 'has a version number' do
     expect(Hair::Api::Client::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe '#image_search' do
+
+    path = '/image/search'
+    query = 'query'
+
+    context '200 status' do
+      sample_response =
+          {
+              status: 200,
+              message: '',
+              data: [
+                  {
+                      image: {
+                          id: 1,
+                          key: 'key',
+                          small: 'http://www.example.com',
+                          medium: 'http://www.example.com',
+                          large: 'http://www.example.com',
+                      },
+                      user: {
+                          id: 1,
+                          name: 'name',
+                          aboutme: 'http://www.example.com',
+                          icon_url: 'http://www.example.com',
+                      },
+                      url: 'http://www.example.com',
+                  }
+              ],
+          }
+
+      it 'returns 200 status' do
+        query_string = URI.encode_www_form({query: query, token: token})
+        WebMock.stub_request(:get, Hair::Api::Client::API_ENDPOINT + path + '?' + query_string).to_return(
+            :body => JSON.generate(sample_response),
+            :status => 200,
+            :headers => {'Content-type' => ' application/json'}
+        )
+        res = Hair::Api::Client.image_search(query)
+        images = res.images
+        expect(images.length).to eq(sample_response[:data].length)
+        expect(res.body['status']).to eq(sample_response[:status])
+        expect(res.body['message']).to eq(sample_response[:message])
+      end
+
+      it 'use query params set by opts' do
+        opts = {
+            page: 2,
+            limit: 10,
+            sort: 'desc',
+            sort_type: 'like',
+        }
+        query_string = URI.encode_www_form(opts.merge({query: query, token: token}))
+        WebMock.stub_request(:get, Hair::Api::Client::API_ENDPOINT + path + '?' + query_string).to_return(
+            :body => JSON.generate(sample_response),
+            :status => 200,
+            :headers => {'Content-type' => ' application/json'}
+        )
+        res = Hair::Api::Client.image_search(query, opts)
+        images = res.images
+        expect(images.length).to eq(sample_response[:data].length)
+        expect(res.body['status']).to eq(sample_response[:status])
+        expect(res.body['message']).to eq(sample_response[:message])
+      end
+    end
+
+    context '500 status' do
+      sample_response =
+          {
+              status: 500,
+              message: 'Internal Server Error.',
+              data: nil
+          }
+
+      it 'returns 500 status' do
+        query_string = URI.encode_www_form({query: query, token: token})
+        WebMock.stub_request(:get, Hair::Api::Client::API_ENDPOINT + path + '?' + query_string).to_return(
+            :body => JSON.generate(sample_response),
+            :status => 500,
+            :headers => {'Content-type' => ' application/json'}
+        )
+        expect { Hair::Api::Client.image_search(query) }.to raise_error(Hair::Api::RequestError)
+      end
+    end
   end
+
 end
